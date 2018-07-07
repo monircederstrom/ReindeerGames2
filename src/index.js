faee7d8be78af39dc92bf213190e7aa4e7f68f60
Chase Pritchett <chase.e.pritchett@gmail.com>
2018-07-07 13:13:59 -0500

    models, routes, and controllers built

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:reindeer/src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -8,12 +7,3 @@ import "./index.css";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> 1a52a41eeb2daa82d437bc6fc88aaa9911111aa2:src/index.js
