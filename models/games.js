20992dd10b3cfb261dacdb0f7967d39e81f64955
Chase Pritchett <chase.e.pritchett@gmail.com>
2018-07-11 18:39:30 -0500

    fixing conflicts

--- models/games.js
+++ models/games.js
@@ -3,16 +3,11 @@ const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
   name: { type: String, required: true },
-<<<<<<< HEAD
-  players: [
-    {
-      user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-=======
   uniqueNum: {type: String },
   players: [
     {
+      // user made into a string again
       user: { type: String, ref: "User" },
->>>>>>> chase-branch
       score: { type: Number, default: 0 },
       photos: {}//something to do with GridFS, files, and chunks. is this doable?
     }
