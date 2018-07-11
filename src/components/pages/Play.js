804b6ca71a8643cea2053b9d69a07c40b30080ef
Amy Schillerstrom <amyschillerstrom@Amys-MacBook-Pro.local>
2018-07-10 20:15:43 -0500

    css changes

--- src/components/pages/Play.js
+++ src/components/pages/Play.js
@@ -42,16 +42,6 @@ class Play extends Component {
                 <div className="play">
             
                    
-<<<<<<< HEAD
-                    <h2 className="title white-text center-align"> Current Challenge Ends:</h2> 
-                    <h5 className=" white-text center-align">  <Countdown  date ={`${year}-07-08T00:00:00`} /></h5>
-                    <Header currentScore={this.state.currentScore} Time={this.state.time}/>  
-                   
-                    <div id="navbuttons">
-                      <a href="/camera" className="waves-effect waves-light btn green white-text center-align " id="save">take a picture</a>
-                      <a href = "/photos" className="waves-effect waves-light btn red white-text center-align " id="save">view my pictures</a>
-                    </div>
-=======
                     <h3 className="title white-text center-align"> Current Challenge Ends:</h3> 
                     <h4 className=" white-text center-align">  <Countdown  date ={`${year}-07-21T00:00:00`} /></h4>
                     <Header currentScore={this.state.currentScore} Time={this.state.time}/>  
@@ -61,7 +51,6 @@ class Play extends Component {
                       <Link to="/camera" className="waves-effect waves-light btn" id="login">Take a Pic!</Link>
                       <Link to="/photos" className="waves-effect waves-light btn" id="login">View My Pics</Link>
                       </div>
->>>>>>> 722c4d41b2649c7e29de40e76df759742a2b87f4
     
                <div className="main-container">
 
