2355a67ba631b100304607219c3f752ba7d9b9aa
Ktlewis17 <klewis.dpt@gmail.com>
2018-07-11 20:35:05 -0500

    changed

--- src/components/pages/CurrentGames.js
+++ src/components/pages/CurrentGames.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import SignInPage from "./SignIn";
-import "./Container.css";
-
-class Game extends Component {
-    
-    render() {
-      return (
-        <div>
-          <main className="container">
-            <p>
-            Explanation of what Reindeer Games is...
-            </p>
-
-            <img className="movingcar" src="assets/images/largelogo1.png" alt="Smiley face" />
-            <div id="loginchoice">
-              <br />
-              <br />
-              <SignInPage />
-            </div>
-          </main>
-          
-        </div>
-      );
-    }
-  }
-  
-  export default Game;
-=======
 import React, { Component } from 'react';
 
 class CurrentGames extends Component {
@@ -43,4 +13,3 @@ class CurrentGames extends Component {
 }
 
 export default CurrentGames;
->>>>>>> 722c4d41b2649c7e29de40e76df759742a2b87f4
--- src/components/pages/Play.js
+++ src/components/pages/Play.js
@@ -17,24 +17,21 @@ class Play extends Component {
         this.state = {
             currentScore: 0,
             Time: Days, Hours , Minutes , Seconds ,
-            Challenge: 0,
-            ReindeerGame: 0
-
-        };
+            ReindeerGame: 0,
+                    
+       
+        onClick(e) {
+            this.setState({
+                count:  this.setState((prevState, props) => ({
+                    counter: prevState.counter + props.increment
+                  })),
+                  
+        })
+    },
+        }
     }
-
- //*******  need to add a function  that will count score  adjust time omce game starts **** ///
-
- //   this.setState({
-   //     currentScore: this.state.currentScore + 1,
-     //   Time: (this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1 : this.state.topScore),
-  //      Challenge: this.state.Challenge + 1,
-    //    ReindeerGame: this.state.ReindeerGame + 1,
-      //       });
+         render () {
           
-  
-
-      render () {
         const currentDate = new Date();
         const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
              return (
@@ -44,6 +41,7 @@ class Play extends Component {
                     <h3 className="title white-text center-align"> Current Challenge Ends:</h3> 
                     <h4 className=" white-text center-align">  <Countdown  date ={`${year}-07-21T00:00:00`} /></h4>
                     <Header currentScore={this.state.currentScore} Time={this.state.time}/>  
+
                    
                     <div id="navbuttons">
                       
@@ -51,24 +49,17 @@ class Play extends Component {
                       <Link to="/photos" className="waves-effect waves-light btn" id="login">View My Pics</Link>
                       </div>
 
-            </div>
-             )
+           <div className="main-container">
 
-               <div className="main-container">
+ 
+              </div>    
+                  </div>
 
+             )}
+               }
+            
 
-                        </div>    
-                                </div> 
-        
-                            );
-                    }
-
-            }
-          }
-          
         
     export default Play;
 
             
-
-    //export default Game;
