2ffa9d519c15b31fcfa11cf18abd4fd25987a8cb
aschillerstrom <33271281+aschillerstrom@users.noreply.github.com>
2018-07-13 20:26:32 -0500

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from 'react-router-dom';
 import { auth, db } from '../../firebase/index';
 //import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as routes from '../../constants/routes';
-import './Container.css';
+
 
 const SignUpPage = ({ history }) =>
   <div>
